95407e65506ffb82ecbb969f8875093e6cc7740d
Fred Alisson <fredalissonsousa@gmail.com>
2023-09-29 18:23:52 -0300

    :recycle: Locale package imported

--- src/com/br/onlineshoppingsystem/application/App.java
+++ src/com/br/onlineshoppingsystem/application/App.java
@@ -2,9 +2,12 @@ package com.br.onlineshoppingsystem.application;
 
 import com.br.onlineshoppingsystem.entities.ShoppingSystem;
 
+import java.util.Locale;
+
 
 public class App {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
         ShoppingSystem shoppingSystem = new ShoppingSystem();
         shoppingSystem.run();

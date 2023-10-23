7c2d8c1287503bbdab9f07078b7db4ff7a933cbd
Rayan <rayanpetros2@gmail.com>
2023-10-23 18:05:47 -0300

    refactor: change run method call logic

--- src/com/br/onlineshoppingsystem/application/App.java
+++ src/com/br/onlineshoppingsystem/application/App.java
@@ -1,10 +1,13 @@
+
 package com.br.onlineshoppingsystem.application;
 
 import com.br.onlineshoppingsystem.services.ShoppingSystemService;
 
 public class App {
+
     public static void main(String[] args) {
         try {
+            //Calling the method directly, so as not create object instance in the method
             new ShoppingSystemService().run();
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
